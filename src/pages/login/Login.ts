e99ee4f3f6baa3b0008588108a8261a95b3afba5
ionetek <ivan@oviland.ru>
2022-06-15 18:07:25 +0300

    updated

--- src/pages/login/Login.ts
+++ src/pages/login/Login.ts
@@ -64,9 +64,6 @@ export default class Login extends Block {
           this.setProps({ emailValue: e.target.value });
           validate(this);
         },
-        focus: () => {
-          validate(this);
-        },
       },
       required: {
         text: 'Некорректный email',
@@ -85,9 +82,6 @@ export default class Login extends Block {
       events: {
         blur: (e: any) => {
           this.setProps({ passwordValue: e.target.value });
-          validate(this);
-        },
-        focus: () => {
           validate(this);
         },
       },
