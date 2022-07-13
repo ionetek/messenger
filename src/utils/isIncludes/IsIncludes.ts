0e66fc41a6c501f1895cd51aaa55ec77612e365a
ionetek <ivan@oviland.ru>
2022-07-13 20:55:47 +0300

    Commit

---
+++ src/utils/isIncludes/IsIncludes.ts
@@ -0,0 +1,38 @@
+type PlainObject<T = any> = {
+    [k in string]: T;
+};
+
+function isPlainObject(value: unknown): value is PlainObject {
+  return typeof value === 'object'
+        && value !== null
+        && value.constructor === Object
+        && Object.prototype.toString.call(value) === '[object Object]';
+}
+
+function isArray(value: unknown): value is [] {
+  return Array.isArray(value);
+}
+
+function isArrayOrObject(value: unknown): value is [] | PlainObject {
+  return isPlainObject(value) || isArray(value);
+}
+
+function isInclude(lhs: PlainObject, rhs: PlainObject) {
+  for (const [key, value] of Object.entries(lhs)) {
+    const rightValue = rhs[key];
+    if (isArrayOrObject(value) && isArrayOrObject(rightValue)) {
+      if (isInclude(value, rightValue)) {
+        continue;
+      }
+      return false;
+    }
+
+    if (value !== rightValue) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export default isInclude;
